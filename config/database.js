c7a68a4a3d460b4a21937a42789893871013f3d6
Bashanta Upreti <ubashanta5@gmail.com>
2021-09-08 16:16:05 +0545

    initial

---
+++ config/database.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const {createPool} = require("mysql");
+//in pool we can reuse the connection
+const pool = createPool({
+    port: process.env.DB_PORT,
+    host:process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB,
+    connectionLimit: 10
+});
+
+module.exports = pool;
